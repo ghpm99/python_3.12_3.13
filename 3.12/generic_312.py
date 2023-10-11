420c0285a2c0a08ef0e505bd9823183cb42e0f34
Guilherme H <ghpm99@gmail.com>
2023-10-11 14:14:00 -0300

    Fix: mypy

--- 3.12/generic_312.py
+++ 3.12/generic_312.py
@@ -5,7 +5,7 @@ type array_string = Sequence[str]
 type generic_array[T: (int, str)] = list[T]
 
 
-def max(list: list[int]) -> None:
+def max(list: array_number) -> None:
     max: int = 0
     for item in list:
         if max < item:
@@ -18,19 +18,19 @@ def print_item[T](list: T) -> None:
     print(list)
 
 
-def sort_item(list: (list[int] | list[str])) -> None:
+def sort_item(list: (array_number | array_string)) -> None:
     sorted_list = sorted(list)
     print(sorted_list)
 
 
-array_number_obj: list[int] = [1, 2, 3, 52, 44, 7]
-array_str_obj: list[str] = ['p', 'y', 't', 'h', 'o', 'n']
+array_number_obj: array_number = [1, 2, 3, 52, 44, 7]
+array_str_obj: array_string = ['p', 'y', 't', 'h', 'o', 'n']
 array_generic_obj = ['T', 4, 'u', 1, True]
 
 
 max(array_number_obj)
 # max(array_str_obj)
-max(array_generic_obj)
+# max(array_generic_obj)
 
 print_item(array_number_obj)
 print_item(array_str_obj)
@@ -38,4 +38,4 @@ print_item(array_generic_obj)
 
 sort_item(array_number_obj)
 sort_item(array_str_obj)
-sort_item(array_generic_obj)
+# sort_item(array_generic_obj)
