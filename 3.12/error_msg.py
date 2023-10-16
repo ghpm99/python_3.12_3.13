382eef89272689495ccfd5c4d36f51f332d00c14
Guilherme H <ghpm99@gmail.com>
2023-10-16 11:50:28 -0300

    Feat: Implements locals comprehension

--- 3.12/error_msg.py
+++ 3.12/error_msg.py
@@ -1,1 +1,9 @@
-sys.version_info
+from itertools import cicle
+
+# sys.version_info
+
+# print_loop(2, key=20, value='print em loop')
+
+# list_attr = []
+
+# list_attr.appends(2)
---
+++ 3.12/local_env.py
@@ -0,0 +1,6 @@
+# PEP 709
+
+a = 1
+name = 'teste'
+
+print([locals() for x in [1]])
