c8743a1464464969a1338430a52d1a1a032f3bd3
Guilherme H <ghpm99@gmail.com>
2023-10-17 16:41:19 -0300

    Feat: Implements mkdtemp

--- 3.12/modules.py
+++ 3.12/modules.py
@@ -1,5 +1,6 @@
 from itertools import batched
 from math import sumprod
+from tempfile import mkdtemp
 
 init_array = [x+1 for x in range(8)]
 result_array = list(batched(init_array, 4))
@@ -8,3 +9,5 @@ print(result_array)
 
 prod = sumprod(result_array[0], result_array[1])
 print(prod)
+
+print(mkdtemp(dir='.'))
