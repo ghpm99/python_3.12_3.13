58b0cb0e6b187967f6c3bbd1a6d0dc1ac184aa0c
Guilherme H <ghpm99@gmail.com>
2023-10-16 23:52:24 -0300

    Feat: Implements more comprehension inlining

--- 3.12/local_env.py
+++ 3.12/local_env.py
@@ -4,3 +4,11 @@ a = 1
 name = 'teste'
 
 print([locals() for x in [1]])
+
+animals = {
+    'boi': 'muuuu',
+    'cachorro': 'au au',
+    'gato': 'miau'
+}
+
+print([f'O {animal} faz {sound}!' for animal, sound in animals.items()])
